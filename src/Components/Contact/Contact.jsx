eb08d0a5545073e23c96d5e72cc0715486f01300
Rahul Bhardwaj <rahulkrbh1122@gmail.com>
2024-09-10 11:32:37 +0530

    ..

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ function Contact() {
   return (
     <div className="contact">
       <div className="contact-title">
-        <h1>Get in touch</h1>
+        <h1>Get in touch with me</h1>
       </div>
       <div className="contact-section">
         <div className="contact-left">
