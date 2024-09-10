cd2a656a24bc20bc4746750a506460608e5ecb84
Rahul Bhardwaj <rahulkrbh1122@gmail.com>
2024-09-10 12:01:04 +0530

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/hero'
 import About from './Components/About/About'
 import Services from './Components/services/Services'
 import MyWork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
+import Hero from './Components/Hero/Hero'
 
 
 
